c543bd94007b6852b56b06ecf3304f53f2e08eb2
YuchanSong <dvlpr_uchan@kakao.com>
2021-12-27 19:53:48 +0900

    Update ViewController.swift

--- swift-location-sample/ViewController.swift
+++ swift-location-sample/ViewController.swift
@@ -9,11 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let ls = LocationService.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        ls.requestAlwaysLocation()
     }
-
-
 }
 
