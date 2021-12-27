6336ff130581d8d612f7e4ded2075374ace7c1a6
YuchanSong <dvlpr_uchan@kakao.com>
2021-12-27 19:09:32 +0900

    Add LocationService.swift

---
+++ swift-location-sample/LocationService.swift
@@ -0,0 +1,16 @@
+//
+//  LocationService.swift
+//  swift-location-sample
+//
+//  Created by ycsong on 2021/12/27.
+//
+
+import Foundation
+
+class LocationService: NSObject {
+    static let shared = LocationService()
+    private override init() { super.init() }
+    
+    
+    
+}
